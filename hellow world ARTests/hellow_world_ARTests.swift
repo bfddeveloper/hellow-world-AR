bf4016d9595e2481f6b304bf124bec8055effebf
brody dickson <bdicksondev@gmail.com>
2024-12-08 16:12:00 -0600

    Initial Commit

---
+++ hellow world ARTests/hellow_world_ARTests.swift
@@ -0,0 +1,17 @@
+//
+//  hellow_world_ARTests.swift
+//  hellow world ARTests
+//
+//  Created by Brody Dickson on 12/8/24.
+//
+
+import Testing
+@testable import hellow_world_AR
+
+struct hellow_world_ARTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
